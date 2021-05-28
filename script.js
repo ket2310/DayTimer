85c05d25f54aa85423037c19eb0465c9db6e3bed
Kirk Thomas <dablumaroon@gmail.com>
2021-05-28 10:24:15 -0500

    We have a calendar.  Now we have to add the functionality to keep track of events during the day.

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+
 var timeFrame = ['past', 'present', 'future'];
 
 var rootEl = $('#root');
-var divHour = $('<div>');
-divHour.addClass('hour col-12 col-md-4 col-lg-3 hour');
+var divHour = new Array();
+var startHour = 9;
+var hourEl = new Array();
+var divTimeFrame = new Array();
+var eventEl = new Array();
+var divSave = new Array();
+var btnSav = new Array();
+var iSave = new Array();
 
-var hourEl = $('<h1>');
-hourEl.text(moment().set('hour', 9).format('h a'));
-hourEl.attr('style', 'text-align: right');
+for (let i = 0; i < 9; i++) {
+    divHour[i] = $('<div>');
+    divHour[i].addClass('hour col-12 col-md-4 col-lg-3 hour');
 
-divHour.append(hourEl);
-rootEl.append(divHour);
+    hourEl[i] = $('<h1>');
+    hourEl[i].text(moment().set('hour', startHour++).format('h a'));
+    hourEl[i].attr('style', 'text-align: right');
 
-var divTimeFrame = $('<div>');
-divTimeFrame.addClass('col-12 col-md-6 col-lg-6 ' + timeFrame[0]);
-var eventEl = $('<h1>');
-eventEl.text('present');
+    divHour[i].append(hourEl[i]);
+    rootEl.append(divHour[i]);
 
-divTimeFrame.append(eventEl);
-rootEl.append(divTimeFrame);
+    divTimeFrame[i] = $('<div>');
+    divTimeFrame[i].addClass('col-12 col-md-6 col-lg-6 ' + timeFrame[0]);
+    eventEl[i] = $('<h1>');
+    eventEl[i].text('present');
 
-var divSave = $('<div>');
-divSave.addClass('class="col-12 col-md-6 col-lg-3 saveBtn');
-var btnSave = $('<button>');
-btnSave.text('save');
-btnSave.attr('style', 'text-align: left; font-size: small');
-divSave.append(btnSave);
-rootEl.append(divSave);
+    divTimeFrame[i].append(eventEl[i]);
+    rootEl.append(divTimeFrame[i]);
+
+    divSave[i] = $('<div>');
+    divSave[i].addClass('class="col-12 col-md-6 col-lg-3 saveBtn bi bi-save2');
+    iSave[i] = $('<i>');
+
+    iSave[i].attr('style', 'text-align: left; font-size: small');
+    divSave[i].append(iSave);
+    rootEl.append(divSave[i]);
+}
